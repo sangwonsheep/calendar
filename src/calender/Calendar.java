9d17512e431db3cb19e917bc7c9b454ed1169bdb
swony <swony@192.168.219.102>
2023-11-22 20:24:27 +0900

    System.out.prinln 활용

--- src/calender/Calendar.java
+++ src/calender/Calendar.java
@@ -3,7 +3,18 @@ package calender;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("----------------");
+		int count = 1;
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 7; j++) {
+				if(count < 10)
+					System.out.print(" ");
+				System.out.print(count + " ");
+				count++;
+			}
+			System.out.println();
+		}
 	}
 
 }
