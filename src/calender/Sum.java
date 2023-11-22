246ac2f8b3e193074d4da0dbfa4570ff50b9e3f1
swony <swony@192.168.219.102>
2023-11-22 20:56:00 +0900

    sum project

---
+++ src/calender/Sum.java
@@ -0,0 +1,17 @@
+package calender;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("두 수를 입력하세요.");
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println("두 수의 합은 " + (a+b) + "입니다.");
+	}
+
+}
