07acc9a19955cb9df188f3911a0e10f6790e78c2
swony <swony@192.168.219.102>
2023-11-24 01:45:25 +0900

    calender update

--- src/calender/Calendar.java
+++ src/calender/Calendar.java
@@ -10,18 +10,13 @@ public class Calendar {
 		return MAX_DAYS[month-1];
 	}
 
-	public void printCalendar() {
-		System.out.println("일 월 화 수 목 금 토");
-		System.out.println("----------------");
-		int count = 1;
-		for(int i = 0; i < 4; i++) {
-			for(int j = 0; j < 7; j++) {
-				if(count < 10)
-					System.out.print(" ");
-				System.out.print(count + " ");
-				count++;
-			}
-			System.out.println();
+	public void printCalendar(int month) {
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("----------------------");
+		for(int i = 1; i <= MAX_DAYS[month-1]; i++) {
+			System.out.printf("%3d", i);
+			if(i % 7 == 0)
+				System.out.println();
 		}
 	}
 
@@ -35,12 +30,13 @@ public class Calendar {
 			System.out.print(PROMPT);
 			int n = input.nextInt();
 			if(n == -1) {
-				System.out.println("Have a nice day!");
+				System.out.println("Bye~");
 				break;
 			}
 			if(n > 12)
 				continue;
-			System.out.println(n + "월은 " + cal.maxDaysOfMonth(n) + "일까지 있습니다.");
+			cal.printCalendar(n);
+			System.out.println();
 		}
 
 	}
