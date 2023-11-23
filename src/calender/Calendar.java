7217b9e42f590ba949c2ccca3f77cbba983a0af4
swony <swony@192.168.219.102>
2023-11-24 02:02:23 +0900

    LEAP_YEAR

--- src/calender/Calendar.java
+++ src/calender/Calendar.java
@@ -5,40 +5,70 @@ import java.util.Scanner;
 public class Calendar {
 
 	private final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-	public int maxDaysOfMonth(int month) {
-		return MAX_DAYS[month-1];
+	public boolean isLeapYear(int year) {
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+		return false;
+	}
+	
+	public int maxDaysOfMonth(int year, int month) {
+		if(isLeapYear(year))
+			return LEAP_MAX_DAYS[month-1];
+		else
+			return MAX_DAYS[month-1];
 	}
 
-	public void printCalendar(int month) {
+	public void printCalendar(int year, int month) {
+		System.out.printf("    <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("----------------------");
-		for(int i = 1; i <= MAX_DAYS[month-1]; i++) {
+		
+		int n;
+		
+		if(isLeapYear(year))
+			n = LEAP_MAX_DAYS[month-1];
+		else
+			n = MAX_DAYS[month-1];
+			
+		for(int i = 1; i <= n; i++) {
 			System.out.printf("%3d", i);
 			if(i % 7 == 0)
 				System.out.println();
 		}
 	}
-
-	public static void main(String[] args) {
+	
+	public void runPrompt() {
 		Scanner input = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		String PROMPT = "> ";
+		
+		int year = 0;
 
 		while(true) {
+			System.out.println("연을 입력하세요");
+			System.out.print(PROMPT);
+			year = input.nextInt();
 			System.out.println("월을 입력하세요");
 			System.out.print(PROMPT);
-			int n = input.nextInt();
-			if(n == -1) {
+			int month = input.nextInt();
+			if(month == -1) {
 				System.out.println("Bye~");
 				break;
 			}
-			if(n > 12)
+			if(month > 12)
 				continue;
-			cal.printCalendar(n);
+			cal.printCalendar(year, month);
 			System.out.println();
 		}
+		
+		input.close();
+	}
 
+	public static void main(String[] args) {
+		Calendar cal = new Calendar();
+		cal.runPrompt();
 	}
 
 }
